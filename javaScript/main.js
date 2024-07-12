3db80aa70160231b927d5d66774a211075531b3a
skeledzicc <nskeledzic@gmail.com>
2024-07-12 14:37:01 +0200

    nova verzija

---
+++ javaScript/main.js
@@ -0,0 +1,75 @@
+const menu = document.querySelector(".navbar-menu");
+const menuItems = document.querySelectorAll(".menuItem"); 
+const hamburger= document.getElementById("hamburgerMenu");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+console.log(hamburger)
+hamburger.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  menu.classList.toggle("showMenu");
+ 
+}
+
+
+// Slider container
+const slider = document.querySelector('.products-container') 
+const slides = document.querySelectorAll('.product-item')
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let current = 0 
+let cycling = true
+
+nextButton.addEventListener('click', function() {
+  slider.scrollBy({
+      left: slider.offsetWidth,
+      behavior: 'smooth'
+  });
+});
+
+prevButton.addEventListener('click', function() {
+  slider.scrollBy({
+      left: -slider.offsetWidth,
+      behavior: 'smooth'
+  });
+});
+
+// dark mode
+
+const root = document.documentElement;
+
+const toggle = document.getElementById("toggle");
+const darkMode = localStorage.getItem("dark-mode");
+if (darkMode) {
+  root.classList.add("dark-theme");
+}
+toggle.addEventListener("click", () => {
+  root.classList.toggle("dark-theme");
+  if (root.classList.contains("dark-theme")) {
+    localStorage.setItem("dark-mode", true);
+  } else {
+    localStorage.removeItem("dark-mode");
+  }
+});
+
+
+//popup
+
+const popup = document.querySelector(".popupBox");
+const close = document.querySelector(".close");
+
+window.onload = function () {
+  setTimeout(() => {
+    popup.style.display = "block";
+  }, 1000);
+};
+
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+
+
+
+
